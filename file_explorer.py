1497e2236a1011b6105feb072dad9833ef998ec5
denzio321 <denzelyeo@gmail.com>
2023-01-23 10:46:15 +0800

    added ability to create a directory by left clicking and typing directory name

--- file_explorer.py
+++ file_explorer.py
@@ -3,11 +3,13 @@ import pygame,commands,time
 WIDTH = 1750
 HEIGHT = 1000
 clicked = True
+
 pygame.init()
+font = pygame.font.Font('freesansbold.ttf', 30)
 class direct(pygame.sprite.Sprite):
     def __init__(self,text,index = None):
         super().__init__()
-        self.image = pygame.font.Font('freesansbold.ttf', 32)
+        self.image = font
         self.dir = text
         self.index = index
         self.text = self.image.render(text, True, (0,0,0),(255,255,255))
@@ -52,12 +54,18 @@ well = 0
 for i in commands.path():
     pwd_group.add(direct(i,well))
     well += 1
-
 running = True
-
+inp_bool = False
 while running:
+    
     if pygame.mouse.get_pressed()[2]:
-            commands.mkdir(input('name'))
+        user_text = 'delete this text before entering dir name'
+        inp = font.render(user_text, True, (0,0,0),(255,255,255))
+        inp_rect = inp.get_rect()
+        inp_rect.topleft = pygame.mouse.get_pos()
+        inp_bool = True
+
+            
     pygame.time.delay(50)
     pwd = commands.run('pwd')
     for text in dir_group:
@@ -75,6 +83,7 @@ while running:
     for i in commands.path():
         pwd_group.add(direct(i,well))
         well += 1
+    
     if not pygame.mouse.get_pressed()[0]:
         clicked = True
     counter_x = 0 
@@ -84,8 +93,17 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN and inp_bool:
+            if event.key == pygame.K_BACKSPACE:
+                user_text = user_text[:-1]
+            elif event.key == pygame.K_RETURN:
+                commands.mkdir(user_text)
+                user_text = ''
+                inp_bool = False
+            else:    
+                user_text += event.unicode
+            inp = font.render(user_text, True, (0,0,0),(255,255,255))
     WIN.fill((0,0,0))
-    
     for text in dir_group:
         text.rect.x = counter_x
         text.rect.y = counter_y
@@ -94,13 +112,17 @@ while running:
             counter_y += 32
             text.rect.x = counter_x
             text.rect.y = counter_y
+    
         WIN.blit(text.text,text.rect)
         counter_x += text.rect.width+10
+    
     for text in pwd_group:
         text.rect.x = path_x
         text.rect.y = path_y
         WIN.blit(text.text,text.rect)
         path_x += text.rect.width+10
+    if inp_bool:
+        WIN.blit(inp,inp_rect)
         
 
 
